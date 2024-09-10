077da9b8b7c5dd6ad33d52a4207a9b1f58b32eb9
Weston C <wes8805@gmail.com>
2024-09-10 11:14:18 -0400

    Update

--- #Calendar App.py
+++ #Calendar App.py
@@ -6,6 +6,11 @@ from tkcalendar import Calendar
 
 application = tk.Tk()
 application.title("Calendar Test")
-calendar = Calendar(application)
+calendar = Calendar(
+application,
+selectmode = "day", 
+firstweekday = 'sunday',                 
+)
 calendar.pack(padx = 15, pady = 15)
+
 application.mainloop()
