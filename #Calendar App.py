03785c43120b1a43d0f6b5653014128b017037cc
Weston C <wes8805@gmail.com>
2024-09-12 12:19:17 -0400

    PyQt Update

--- #Calendar App.py
+++ #Calendar App.py
@@ -1,13 +1,40 @@
 
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import (
+    QApplication, 
+    QMainWindow,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 def app():
     application = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
     window.setGeometry(0, 0, 2000, 2000)
     window.setWindowTitle("The Python Calendar")
+    widget = QLabel("Test")
+    font = widget.font()
+    font.setPointSize(70)
+    widget.setFont(font)
+    widget.setAlignment
+  
     window.show()
     sys.exit(application.exec_())
 
